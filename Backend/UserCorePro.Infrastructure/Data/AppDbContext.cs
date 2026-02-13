4be7ad4a08e520fd975556223683e9cc1020d4d4
umutozer <42037087+umutozer@users.noreply.github.com>
2026-02-13 16:12:21 +0300

    feat: Backend/UserCorePro.Infrastructure/Data/AppDbContext.cs

---
+++ Backend/UserCorePro.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using UserCorePro.Core.Entities;
+
+namespace UserCorePro.Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasIndex(u => u.Email).IsUnique();
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
